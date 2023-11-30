f6fb57d7d08cc33dd84c472634d02a7b92bee8f3
Goodjop <sashakinser2005@mail.ru>
2023-11-30 10:19:24 +0400

    final_2

---
+++ main_window.py
@@ -0,0 +1,245 @@
+import sys
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QDate
+
+import modules.search as ds
+import modules.data_iterator as d_iter
+import modules.distribution_data as dd
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(648, 501)
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        MainWindow.setFont(font)
+
+
+        self.path = ""
+        self.it = None
+
+     
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.setStyleSheet(
+            "background-color: rgb(100, 149, 237);")   
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(20, 310, 613, 155))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setContentsMargins(1, 1, 10, 1)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_2.setContentsMargins(0, 10, 80, 10)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        self.input_descr = QtWidgets.QLabel(self.verticalLayoutWidget)
+
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        font.setBold(False)
+        font.setWeight(50)
+        self.input_descr.setFont(font)
+        self.input_descr.setObjectName("input_descr")
+        self.horizontalLayout_2.addWidget(self.input_descr)
+
+
+        self.date = QtWidgets.QDateEdit(self.verticalLayoutWidget)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.date.setFont(font)
+        self.date.setDate(QtCore.QDate(2008, 1, 10))
+        self.date.setStyleSheet(
+            "background-color: rgb(0, 191, 255);")
+        self.date.setObjectName("date")
+        self.horizontalLayout_2.addWidget(self.date)
+        self.horizontalLayout.addLayout(self.horizontalLayout_2)
+        self.btn_get_data = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.btn_get_data.clicked.connect(self.get_data)
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        font.setBold(True)
+        font.setWeight(75)
+        self.btn_get_data.setFont(font)
+        self.btn_get_data.setStyleSheet(
+            "background-color: rgb(0, 191, 255);")
+        self.btn_get_data.setObjectName("btn_get_data")
+        self.horizontalLayout.addWidget(self.btn_get_data)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        self.info = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.info.setObjectName("info")
+        self.verticalLayout.addWidget(self.info)
+        self.next_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.next_button.setStyleSheet(
+            "background-color: rgb(0, 191, 255);")
+        self.next_button.setObjectName("next_button")
+        self.next_button.clicked.connect(self.next_element)
+
+        self.verticalLayout.addWidget(self.next_button)
+        self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setGeometry(
+            QtCore.QRect(130, 50, 378, 178))
+        self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(
+            self.verticalLayoutWidget_2)
+        self.verticalLayout_3.setContentsMargins(-10, 50, 0, 0)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+        self.div_text_label = QtWidgets.QLabel(self.verticalLayoutWidget_2)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        font.setBold(True)
+        font.setWeight(75)
+        self.div_text_label.setFont(font)
+        self.div_text_label.setTextFormat(QtCore.Qt.AutoText)
+        self.div_text_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.div_text_label.setIndent(10)
+        self.div_text_label.setObjectName("div_text_label")
+        self.verticalLayout_3.addWidget(self.div_text_label)
+        self.btn_div_by_data_date = QtWidgets.QPushButton(
+            self.verticalLayoutWidget_2)
+        self.btn_div_by_data_date.setStyleSheet(
+            "background-color: rgb(0, 191, 255);")
+        self.btn_div_by_data_date.setObjectName("btn_div_by_data_date")
+        self.btn_div_by_data_date.clicked.connect(self.div_by_data_date)
+        self.verticalLayout_3.addWidget(self.btn_div_by_data_date)
+        self.btn_div_by_week = QtWidgets.QPushButton(
+            self.verticalLayoutWidget_2)
+        self.btn_div_by_week.setStyleSheet(
+            "background-color: rgb(0, 191, 255);")
+        self.btn_div_by_week.setObjectName("btn_div_by_week")
+        self.verticalLayout_3.addWidget(self.btn_div_by_week)
+        self.btn_div_by_week.clicked.connect(self.div_by_week)
+        self.btn_div_by_year = QtWidgets.QPushButton(
+            self.verticalLayoutWidget_2)
+        self.btn_div_by_year.setStyleSheet(
+            "background-color: rgb(0, 191, 255);")
+        self.btn_div_by_year.setObjectName("btn_div_by_year")
+        self.btn_div_by_year.clicked.connect(self.div_by_year)
+        self.verticalLayout_3.addWidget(self.btn_div_by_year)
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setStyleSheet(
+            "background-color: rgb(0, 191, 255);")        
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 625, 51))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.upload_path)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        self.statusbar.setStyleSheet(
+            "background-color: rgb(100, 149, 237);")    
+        MainWindow.setStatusBar(self.statusbar)
+        self.pushButton.setStyleSheet(
+            "background-color: rgb(0, 191, 255);")   
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.input_descr.setText(_translate("MainWindow", "Введите дату:"))
+        self.btn_get_data.setText(_translate(
+            "MainWindow", "Получить информацию"))
+        self.info.setText(_translate("MainWindow", "Температура: -20 °C\n"
+                                     "Давление: 765 мм.рт.ст.\n"
+                                     "Ветер: В 2 м/c"))
+        self.next_button.setText(_translate("MainWindow", " Следующая дата"))
+        self.div_text_label.setText(_translate(
+            "MainWindow", "Предложенные функции разделения"))
+        self.btn_div_by_data_date.setText(_translate(
+            "MainWindow", " На файлы с датой и данными "))
+        self.btn_div_by_week.setText(_translate(
+            "MainWindow", "По неделям"))
+        self.btn_div_by_year.setText(_translate(
+            "MainWindow", "По годам"))
+        self.pushButton.setText(_translate(
+            "MainWindow", "Загрузить файл(dataset)"))
+
+
+    def upload_path(self):
+        self.path = QtWidgets.QFileDialog.getOpenFileName()[0]
+        if self.path:
+            try:
+                self.it = d_iter.DataIterator(self.path)
+            except:
+                self.path = ""
+                self.__warning_icon(
+                    "Ахтунг!", "Вы выбрали файл не того формата")
+
+    def get_data(self):
+        try:
+            if self.path:
+                data = ds.search(self.path, self.date.date().toPyDate())
+                self.info.setText(
+                    f"Температура: {data[0]} °C\nДавление: {data[1]} мм.рт.ст.\nВетер: {data[2]} {data[3]} м/c")
+            else:
+                self.__warning_icon(
+                    "Ахтунг!", "Загрузите файл с исходным dataset")
+        except:
+            self.info.setText("Информация за этот день отсутствует")
+
+    def next_element(self):
+        try:
+            self.it.index = ds.find(self.path, self.date.date().toPyDate())
+            data = next(self.it)
+            self.info.setText(
+                f"Температура: {data[1]} °C\nДавление: {data[2]} мм.рт.ст.\nВетер: {data[3]} {data[4]} м/c")
+            date = list(map(int, data[0].split("-")))
+            self.date.setDate(QDate(*date))
+        except StopIteration:
+            self.__warning_icon(
+                "Ахтунг!", "Элементов в датесете больше нет")
+        except:
+            self.__warning_icon(
+                "Ахтунг!", "Загрузите файл с исходным dataset")
+
+    def div_by_data_date(self):
+        folderpath = QtWidgets.QFileDialog.getExistingDirectory()
+        if folderpath:
+            dd.distribution_date_and_data(folderpath, self.path)
+        else:
+            self.__warning_icon(
+                "Ахтунг!", "Укажите папку, куда сохранить разделенные файлы")
+            
+    def div_by_week(self):
+        folderpath = QtWidgets.QFileDialog.getExistingDirectory()
+        if folderpath:
+            dd.distribution_by_week(folderpath, start_day=4, file_path=self.path)
+        else:
+            self.__warning_icon(
+                "Ахтунг!", "Укажите папку, куда сохранить разделенные файлы")    
+
+
+    def div_by_year(self):
+        try:
+            folderpath = QtWidgets.QFileDialog.getExistingDirectory()
+            if folderpath:
+                dd.distribution_by_year(folderpath, self.path)
+            else:
+                self.__warning_icon(
+                    "Ахтунг!", "Укажите папку, куда сохранить разделенные файлы")
+        except:
+            self.__warning_icon(
+                "Ахтунг!", "Загрузите файл с исходными данными")
+
+    def __warning_icon(self, text, info):
+        error = QtWidgets.QMessageBox()
+        error.setIcon(QMessageBox.Warning)
+        error.setWindowTitle(text)
+        error.setText(info)
+        error.exec_()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
--- modules/distribution_data.py
+++ modules/distribution_data.py
@@ -1,6 +1,6 @@
 import csv
+import datetime
 import os
-import pandas as pd
 
 def read_data(file_path: str) -> list:
     data = []
@@ -10,56 +10,65 @@ def read_data(file_path: str) -> list:
             data.append(row)
     return data
 
-def distribution_date_and_data(directory_path: str) -> None:
+
+def growth(date1: str, date2: str) -> int:
+    current_day = datetime.datetime.strptime(date1, '%Y-%m-%d').date()
+    next_day = datetime.datetime.strptime(date2, '%Y-%m-%d').date()
+    return (next_day - current_day).days
+
+
+def distribution_date_and_data(directory_path: str, file_path: str) -> None:
     path = os.getcwd()
     os.chdir(directory_path)
-    data = read_data(r"C:\Ycheba\2_kurs\LAB_Python\datasets\dataset.csv")
-
-    x_data = [[i[0].split("-")] for i in data]
+    data = read_data(file_path)
+    x_data = [[i[0].split("-")] for i in data]  
+    y_data = [i[1:] for i in data]  
     with open('X.csv', 'w', encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerows(x_data)  
-
-    y_data = [i[1:] for i in data]    
     with open('Y.csv', 'w', encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerows(y_data)
     os.chdir(path)
 
 
-def distribution_by_year(directory_path: str) -> None:
-    data = read_data(r"C:\Ycheba\2_kurs\LAB_Python\datasets\dataset.csv") 
-    print (data)     
+def distribution_by_year(directory_path: str, file_path: str) -> None:
+    data = read_data(file_path)
     path = os.getcwd()
     os.chdir(directory_path)
-    
-    for year in data:
-        year_data = data[data['date'] == year]
-        output_file = f'{directory_path}/{year_data.iloc[0]["date"].strftime("%Y%m%d")}_{year_data.iloc[-1]["date"].strftime("%Y%m%d")}.csv'
-    with open(output_file, 'w', encoding="utf-8", newline="") as file:
+    year_list = []
+    for day in data:
+        year_list.append(int(day[0].split("-")[0]))
+    year_list = sorted(list(set(year_list)))
+    for year in year_list:
+        data_year = [day for day in data if int(day[0].split("-")[0]) == year]
+        a = "".join(data_year[0][0].split("-"))
+        b = "".join(data_year[-1][0].split("-"))
+        with open(f"{a}_{b}.csv", 'w', encoding="utf-8", newline="") as file:
             writer = csv.writer(file)
-            writer.writerows(year_data)
+            writer.writerows(data_year)
     os.chdir(path)
 
-def distribution_by_week(directory_path: str,file_path:str, start_day ) -> None:
-    data = read_data(r"C:\Ycheba\2_kurs\LAB_Python\datasets\dataset.csv")
+def distribution_by_week(directory_path: str, start_day: int, file_path: str, ) -> None:
+    data = read_data(file_path)
     path = os.getcwd()
     os.chdir(directory_path)
-    
-    for week_start in data['date']:
-        week_end = week_start
-        week_data = data[(data['date'] >= week_start) & (data['date'] <= week_end)]
-        output_file = f'{directory_path}/{week_data.iloc[0]["date"].strftime("%Y%m%d")}_{week_data.iloc[-1]["date"].strftime("%Y%m%d")}.csv'
-    with open(output_file, 'w', encoding="utf-8", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerows(week_data)        
+    day_of_week = start_day
+    data_week = [data[0]]
+    for i in range(1, len(data) - 1):
+        if day_of_week >= 7:
+            a = "".join(data_week[0][0].split("-"))
+            b = "".join(data_week[-1][0].split("-"))
+            with open(f"{a}_{b}.csv", 'w', encoding="utf-8", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerows(data_week)
+            data_week.clear()
+            data_week.append(data[i])
+            day_of_week = growth(data[i-1][0], data[i][0])
+            continue
+        data_week.append(data[i])
+        day_of_week += growth(data[i-1][0], data[i][0])
     os.chdir(path)
-
-if __name__ == "__main__":
-    
-    distribution_by_year(r"C:\Ycheba\2_kurs\LAB_Python\datasets")
-
-
 
 
 
