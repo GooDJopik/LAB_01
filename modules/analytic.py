9af2227f1d2da6b8fa0b0501fbf77239a5082d05
Goodjop <sashakinser2005@mail.ru>
2023-12-18 23:29:56 +0400

    4 lab

--- modules/analytic.py
+++ modules/analytic.py
@@ -1,22 +1,16 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def process_data(path: str) -> pd.DataFrame:
-    """
-    Считывание и обработка в соответствии с вариантом данных из набора данных
-    Args:
-      path: путь к файлу для считывания информации
-    Returns:
-      Dataframe без значений Nan и с добавлением столбца температуры по Фаренгейту
-    """
-    df = pd.read_csv(path)
-    df.columns = ["Date", "Celsius temperature", "Pressure", "Direction", "Speed"]
-    df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
-    df = df.dropna().reset_index(drop=True)
-    df['Fahrenheit temperature'] = 9/5 * df['Celsius temperature'] + 32
-    return df
+df = pd.read_csv('datasets/dataset.csv')
+
+df.columns = ["Date", "Celsius temperature", "Pressure", "Direction", "Speed"]
+
+df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
+
+df = df.dropna().reset_index(drop=True)
+
+df['Fahrenheit temperature'] = 9/5 * df['Celsius temperature'] + 32
 
 
 def describe_column(df: pd.DataFrame, parametr: str) -> pd.Series | None:
@@ -60,6 +54,76 @@ def filter_by_date(df: pd.DataFrame, end_date: str, start_date: str) -> pd.DataF
     end_date = pd.to_datetime(end_date, format='%Y-%m-%d')
     return df[(start_date <= df["Date"]) & (df["Date"] <= end_date)]
 
+
+def average_monthly_temperature(df: pd.DataFrame, parametr: str) -> pd.Series | None:
+    """
+    Группировка по месяцам с вычислением среднего значения температуры
+    Args:
+      df: Dataframe с исходными значениями
+      parametr: Столбец, указывающий, какая температура берется
+    Returns:
+      Ряд, показывающий среднее значение за все месяцы
+
+    """
+    if parametr in ["Celsius temperature", 'Fahrenheit temperature']:
+        return df.groupby(df.Date.dt.month)[parametr].mean()
+
+
+def show_temperature_graph(df: pd.DataFrame, parametr: str) -> None:
+    """
+    Отображение графика температуры за весь период
+    Args:
+      df: Dataframe с исходными значениями
+      parametr: Столбец, указывающий, какая температура берется
+
+    """
+    if parametr in ["Celsius temperature", 'Fahrenheit temperature']:
+        fig = plt.figure(figsize=(30, 5))
+        plt.ylabel(parametr)
+        plt.xlabel("date")
+        plt.title('Изменение температуры')
+        plt.plot(df["Date"], df[parametr], color='green', linestyle='-', linewidth=2)
+        plt.show()
+
+
+def show_monthly_temperature_statistics(df: pd.DataFrame, month: int, year: int) -> None:
+    """
+    Отображение графика температуры за указанный месяц в году
+    и отображение медианы и средних значений
+    Args:
+      df: Dataframe с исходными значениями
+      month: Месяц, для которого строится график температуры
+      year: Год, для которого строится график температуры
+      
+    """
+    month_df = df[(df.Date.dt.month == month) & (df.Date.dt.year == year)]
+    fig = plt.figure(figsize=(30, 5))
+
+    fig.add_subplot(1, 3, 1)
+    plt.ylabel("Celsius temperature")
+    plt.xlabel("Date")
+    plt.plot(month_df.Date.dt.day, month_df["Celsius temperature"],
+             color='green', linestyle='-', linewidth=2, label='Celsius temperature')
+    plt.axhline(y=month_df["Celsius temperature"].mean(), color='yellow', label="Average value")
+    plt.axhline(y=month_df["Celsius temperature"].median(), color='blue', label="Median")
+    plt.legend(loc=2, prop={'size': 10})
+
+    fig.add_subplot(1, 3, 2)
+    plt.ylabel("Fahrenheit temperature")
+    plt.xlabel("Date")
+    plt.plot(month_df.Date.dt.day, month_df["Fahrenheit temperature"],
+             color='red', linestyle='--', linewidth=2, label='Fahrenheit temperature')
+    plt.axhline(y=month_df["Fahrenheit temperature"].mean(), color='yellow', label="Average value")
+    plt.axhline(y=month_df["Fahrenheit temperature"].median(), color='blue', label="Median")
+    plt.legend(loc=2, prop={'size': 10})
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    filtered_temp_df = filter_celsius_temp(df, 25)
+    filtered_date_df = filter_by_date(df, '2023-01-01', '2023-12-31')
+    show_monthly_temperature_statistics(df, 1, 2023)
 
 
 
